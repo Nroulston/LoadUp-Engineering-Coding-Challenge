7556701616d3a1efbd7cf29a73dd7354ecf9c0f8
Nroulston <nicolas.roulston@gmail.com>
2021-01-21 15:11:53 -0700

    setup first stimulus controller and action

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -12,7 +12,7 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "output" ]
 
-  connect() {
+  greet() {
     this.outputTarget.textContent = 'Hello, Stimulus!'
   }
 }
---
+++ app/javascript/controllers/location_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "output" ]
+
+  fetch() {
+    
+  }
+  
+}
+// this.outputTarget.textContent = 'Hello, Stimulus!'
