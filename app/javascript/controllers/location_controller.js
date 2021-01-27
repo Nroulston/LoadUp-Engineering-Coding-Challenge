b1675186ab1c82523e7859c8ed7455e9f9bcf647
Nroulston <nicolas.roulston@gmail.com>
2021-01-27 14:55:14 -0700

    Get forecast data and send to weather controller
    Convert the fetch response to just the json data that we need, use
    axios to send an async request to a not yet made weather controller
    with the array of forecasts.

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -15,9 +15,10 @@ export default class extends Controller {
    let data = await response.json()   
 
    let weather_response = await fetch(`https://api.weather.gov/gridpoints/${data.properties.gridId}/${data.properties.gridX},${data.properties.gridY}/forecast`)
-   let forecast = await weather_response.json()
+   let forecast_response = await weather_response.json()
+   let forecast_periods = forecast_response.properties.periods 
+   const forecast = await axios("/weather", {params: { forecast_periods } })
    debugger
-   
   }
 
   
