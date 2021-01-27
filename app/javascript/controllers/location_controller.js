0db8829e732a03ca8329c5c1398d1cf21dc9e0d2
Nroulston <nicolas.roulston@gmail.com>
2021-01-27 12:47:28 -0700

    grab location and weather data for the user

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -2,13 +2,37 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = [ "output" ]
-
+  
   click() {
     navigator.geolocation.getCurrentPosition(this.success)
   }
-
+  
   success(position) {
-    console.log(position.coords.latitude)
-    console.log(position.coords.longitude)
+   console.log(position.coords.latitude)
+   console.log(position.coords.longitude)
+   fetch(`https://api.weather.gov/points/${position.coords.latitude},${position.coords.longitude}`)
+      .then(response => response.json())
+      .then(data => {
+        fetch(`https://api.weather.gov/gridpoints/${data.properties.gridId}/${data.properties.gridX},${data.properties.gridY}/forecast`)
+          .then(response => {
+             
+           })
+          
+      })
+          
+
+    
   }
+
+  
+  
+  //async setPosition(position) {
+    //   try {
+  //     let grid = await fetch(`https://api.weather.gov/points/${position.coords.latitude},${position.coords.longitude}`)
+  //     debugger
+  //   }
+  //   catch(e) {
+  //     console.log(e)
+  //   }
+  // }
 }
