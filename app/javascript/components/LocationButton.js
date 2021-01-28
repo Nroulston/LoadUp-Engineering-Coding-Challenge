9da6d822d44cde00a67f55c5ae158ab1d396efc4
Nroulston <nicolas.roulston@gmail.com>
2021-01-27 23:14:38 -0700

    clean up component

--- app/javascript/components/LocationButton.js
+++ app/javascript/components/LocationButton.js
@@ -1,18 +1,4 @@
 import React from "react"
-import PropTypes from "prop-types"
-// class LocationButton extends React.Component {
-//   render () {
-//     return (
-//       <React.Fragment>
-//          <button data-action='click->location#click'>Current Weather</button>
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-
-// export default LocationButton
-
 
 export default function LocationButton(){
   return (
